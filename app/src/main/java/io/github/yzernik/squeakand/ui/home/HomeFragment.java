ab9c6b6d07f20320c1c784ce60f4262da40cc3f2
yzernik <yzernik@gmail.com>
2020-05-10 13:53:21 -0700

    Got list working with todo items, but still word styles

--- app/src/main/java/io/github/yzernik/squeakand/ui/home/HomeFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/home/HomeFragment.java
@@ -20,17 +20,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
-import io.github.yzernik.squeakand.NewWordActivity;
 import io.github.yzernik.squeakand.R;
-import io.github.yzernik.squeakand.Word;
-import io.github.yzernik.squeakand.WordListAdapter;
+import io.github.yzernik.squeakand.Todo;
+import io.github.yzernik.squeakand.TodoListAdapter;
 
 import static android.app.Activity.RESULT_OK;
 
 
 public class HomeFragment extends Fragment {
 
-    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    public static final int NEW_TODO_ACTIVITY_REQUEST_CODE = 1;
 
     private HomeViewModel homeViewModel;
 
@@ -41,48 +40,50 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         final RecyclerView recyclerView = root.findViewById(R.id.recyclerview);
-        final WordListAdapter adapter = new WordListAdapter(root.getContext());
+        final TodoListAdapter adapter = new TodoListAdapter(root.getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
 
         // Get a new or existing ViewModel from the ViewModelProvider.
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // Add an observer on the LiveData returned by getAlphabetizedTodos.
         // The onChanged() method fires when the observed data changes and the activity is
         // in the foreground.
-        homeViewModel.getAllWords().observe(getViewLifecycleOwner(), new Observer<List<Word>>() {
+        homeViewModel.getAllTodos().observe(getViewLifecycleOwner(), new Observer<List<Todo>>() {
             @Override
-            public void onChanged(@Nullable final List<Word> words) {
-                // Update the cached copy of the words in the adapter.
-                adapter.setWords(words);
+            public void onChanged(@Nullable final List<Todo> todos) {
+                // Update the cached copy of the todos in the adapter.
+                adapter.setTodos(todos);
             }
         });
 
-        FloatingActionButton fab = root.findViewById(R.id.fab);
+        // TODO: uncomment
+/*        FloatingActionButton fab = root.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getContext(), NewWordActivity.class);
-                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+                Intent intent = new Intent(getContext(), NewTodoActivity.class);
+                startActivityForResult(intent, NEW_TODO_ACTIVITY_REQUEST_CODE);
             }
-        });
+        });*/
 
         return root;
     }
 
-    @Override
+    // TODO: uncomment
+/*    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
-            homeViewModel.insert(word);
+        if (requestCode == NEW_TODO_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Todo todo = new Todo(data.getStringExtra(NewTodoActivity.EXTRA_REPLY));
+            homeViewModel.insert(todo);
         } else {
             Toast.makeText(
                     getContext(),
                     R.string.empty_not_saved,
                     Toast.LENGTH_LONG).show();
         }
-    }
+    }*/
 }
