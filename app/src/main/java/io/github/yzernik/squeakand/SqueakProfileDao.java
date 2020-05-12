ce009ef57d7c0ecfb4f10395f6d7af175eccc5f4
Jonathan Zernik <yzernik@gmail.com>
2020-05-11 20:03:48 -0700

    Add SqueakProfile class to db (#12)

---
+++ app/src/main/java/io/github/yzernik/squeakand/SqueakProfileDao.java
@@ -0,0 +1,41 @@
+package io.github.yzernik.squeakand;
+
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SqueakProfileDao {
+
+    @Query("SELECT * from " + TodoRoomDatabase.TABLE_NAME_PROFILE + " ORDER BY profile_id ASC")
+    LiveData<List<SqueakProfile>> getProfiles();
+
+    @Query("SELECT * FROM " + TodoRoomDatabase.TABLE_NAME_PROFILE + " WHERE profile_id = :profileId")
+    LiveData<SqueakProfile> fetchProfileById(int profileId);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(SqueakProfile squeakProfile);
+
+    @Query("DELETE FROM " + TodoRoomDatabase.TABLE_NAME_PROFILE)
+    void deleteAll();
+}
