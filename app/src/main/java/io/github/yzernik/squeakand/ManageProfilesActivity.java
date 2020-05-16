19750bd89d19751cd459473e4381457cd2788433
yzernik <yzernik@gmail.com>
2020-05-16 03:15:42 -0700

    Fix fragment for manage profiles activity

--- app/src/main/java/io/github/yzernik/squeakand/ManageProfilesActivity.java
+++ app/src/main/java/io/github/yzernik/squeakand/ManageProfilesActivity.java
@@ -3,11 +3,6 @@ package io.github.yzernik.squeakand;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import io.github.yzernik.squeakand.ui.profile.ManageProfilesFragment;
-import io.github.yzernik.squeakand.ui.profile.SelectProfileFragment;
 
 public class ManageProfilesActivity extends AppCompatActivity {
 
@@ -15,19 +10,6 @@ public class ManageProfilesActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_profiles);
-
-        Bundle bundle = new Bundle();
-        // Create new fragment and transaction
-        Fragment newFragment = new ManageProfilesFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack
-        newFragment.setArguments(bundle);
-        // int currentContainerViewId = ((ViewGroup) getView().getParent()).getId();
-        transaction.replace(R.id.manage_profiles_fragment, newFragment);
-        // transaction.addToBackStack(null);
-        // Commit the transaction
-        transaction.commit();
     }
 
 }
