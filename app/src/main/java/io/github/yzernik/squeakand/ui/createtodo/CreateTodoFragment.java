99723530fd12c6e82a180cdc54d993e939d152b8
yzernik <yzernik@gmail.com>
2020-05-18 13:42:05 -0700

    Create single model for create todo fragment

--- app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,7 +25,6 @@ import java.util.List;
 
 import io.github.yzernik.squeakand.R;
 import io.github.yzernik.squeakand.SqueakProfile;
-import io.github.yzernik.squeakand.ui.selectprofile.SelectProfileModel;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
@@ -38,7 +36,7 @@ public class CreateTodoFragment extends Fragment {
     private TextInputLayout mTextInput;
     private Button button;
 
-    private SelectProfileModel selectProfileModel;
+    private CreateTodoModel selectProfileModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -47,7 +45,7 @@ public class CreateTodoFragment extends Fragment {
         mTextInput = root.findViewById(R.id.squeak_text);
         button = root.findViewById(R.id.btnDone);
 
-        selectProfileModel = new ViewModelProvider(getActivity()).get(SelectProfileModel.class);
+        selectProfileModel = new ViewModelProvider(getActivity()).get(CreateTodoModel.class);
 
         selectProfileModel.getSelectedSqueakProfile().observe(getViewLifecycleOwner(), new Observer<SqueakProfile>() {
             @Override
