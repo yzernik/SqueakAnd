fda921d670f288c598566a3cf4064c7df58cd532
yzernik <yzernik@gmail.com>
2020-05-18 19:46:05 -0700

    Remove toast on select profile

--- app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -131,7 +130,6 @@ public class CreateTodoFragment extends Fragment {
             public void onClick(DialogInterface dialog, int which) {
                 SqueakProfile selectedProfile = profiles.get(which);
                 createTodoModel.setSelectedSqueakProfileId(selectedProfile.getProfileId());
-                Toast.makeText(getContext(), "Selected profile " + selectedProfile.getName(), Toast.LENGTH_SHORT).show();
             }
         });
         // create and show the alert dialog
