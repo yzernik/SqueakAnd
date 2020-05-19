1c0f5a35ac8defc1dcee56769b7b47e778b1df47
yzernik <yzernik@gmail.com>
2020-05-18 17:56:42 -0700

    Make create_profile a single fragment that uses keypair in the model

--- app/src/main/java/io/github/yzernik/squeakand/ui/createprofile/CreateProfileFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/createprofile/CreateProfileFragment.java
@@ -1,5 +1,6 @@
 package io.github.yzernik.squeakand.ui.createprofile;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,42 +11,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import org.bitcoinj.core.ECKey;
+import org.bitcoinj.params.MainNetParams;
+
+import java.util.List;
 
 import io.github.yzernik.squeakand.R;
 import io.github.yzernik.squeakand.SqueakProfile;
-import io.github.yzernik.squeakand.ui.profile.ManageProfilesModel;
+import io.github.yzernik.squeaklib.core.Signing;
 
 public class CreateProfileFragment extends Fragment implements GeneratePrivateKeyDialogFragment.GeneratePrivateKeyNoticeDialogListener {
 
-    TextView mShowNewProfileName;
+    TextInputLayout mProfileNameInput;
     Button mGeneratePrivateKeyButton;
     Button mImportPrivateKeyButton;
+    TextView mProfileAddress;
 
-    private ManageProfilesModel manageProfilesModel;
+    private CreateProfileModel createProfileModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_create_profile, container, false);
 
-        mShowNewProfileName = root.findViewById(R.id.create_profile_new_profile_name);
+        mProfileNameInput = root.findViewById(R.id.new_profile_name_input);
         mGeneratePrivateKeyButton = root.findViewById(R.id.create_profile_generate_private_key_button);
         mImportPrivateKeyButton = root.findViewById(R.id.create_profile_import_private_key_button);
+        mProfileAddress = root.findViewById(R.id.new_profile_address);
 
-        // Get a new or existing ViewModel from the ViewModelProvider.
-        manageProfilesModel = new ViewModelProvider(getActivity()).get(ManageProfilesModel.class);
+        createProfileModel = new ViewModelProvider(getActivity()).get(CreateProfileModel.class);
 
-        // String profileName = "";
-        Bundle arguments = getArguments();
-        final String profileName = this.getArguments().getString("name");
-
-        DialogFragment dialog = new GeneratePrivateKeyDialogFragment(profileName, this);
-
-        mShowNewProfileName.setText(profileName);
+        DialogFragment dialog = new GeneratePrivateKeyDialogFragment("", this);
 
         mGeneratePrivateKeyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,10 +58,28 @@ public class CreateProfileFragment extends Fragment implements GeneratePrivateKe
             }
         });
 
+        // Show an alert dialog when the import private key button is clicked.
         mImportPrivateKeyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(getTag(), "Import private key here.");
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                alertDialog.setTitle("Import private key not supported.");
+                alertDialog.setMessage("Importing private keys is not currently supported.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+
+        createProfileModel.getKeyPair().observe(getActivity(), new Observer<Signing.KeyPair>() {
+            @Override
+            public void onChanged(@Nullable final Signing.KeyPair keyPair) {
+                mProfileAddress.setText(keyPair.getPublicKey().getAddress(MainNetParams.get()));
             }
         });
 
@@ -65,18 +87,18 @@ public class CreateProfileFragment extends Fragment implements GeneratePrivateKe
         return root;
     }
 
-    private void createProfileWithPrivateKey(String profileName) {
+    private void generateKeyPair(String profileName) {
         Log.i(getTag(), "Generate private key here.");
         ECKey ecKey = new ECKey();
-        SqueakProfile squeakProfile = new SqueakProfile(profileName, ecKey);
-        manageProfilesModel.insert(squeakProfile);
-        Toast.makeText(getContext(), "Created profile " + profileName, Toast.LENGTH_SHORT).show();
+        // SqueakProfile squeakProfile = new SqueakProfile(profileName, ecKey);
+        // createProfileModel.insert(squeakProfile);
+        // Toast.makeText(getContext(), "Created profile " + profileName, Toast.LENGTH_SHORT).show();
+        createProfileModel.setmKeyPair(ecKey);
     }
 
     @Override
     public void onDialogPositiveClick(DialogFragment dialog, String profileName) {
-        createProfileWithPrivateKey(profileName);
-        getActivity().finish();
+        generateKeyPair(profileName);
     }
 
     @Override
---
+++ app/src/main/java/io/github/yzernik/squeakand/ui/createprofile/CreateProfileModel.java
@@ -0,0 +1,51 @@
+package io.github.yzernik.squeakand.ui.createprofile;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import org.bitcoinj.core.ECKey;
+
+import java.util.List;
+
+import io.github.yzernik.squeakand.SqueakProfile;
+import io.github.yzernik.squeakand.SqueakProfileRepository;
+import io.github.yzernik.squeaklib.core.Signing;
+
+public class CreateProfileModel  extends AndroidViewModel {
+
+    private SqueakProfileRepository mRepository;
+    private LiveData<List<SqueakProfile>> mAllSqueakProfiles;
+    // private MutableLiveData<KeyPair> mKeyPair;
+    private MutableLiveData<ECKey> mECKey;
+
+    public CreateProfileModel(Application application) {
+        super(application);
+        mRepository = new SqueakProfileRepository(application);
+        mAllSqueakProfiles = mRepository.getAllSqueakProfiles();
+        mECKey = new MutableLiveData<>();
+    }
+
+    public void setmKeyPair(ECKey ecKey) {
+        this.mECKey.setValue(ecKey);
+    }
+
+    public LiveData<List<SqueakProfile>> getmAllSqueakProfiles() {
+        return mAllSqueakProfiles;
+    }
+
+    public void insert(SqueakProfile squeakProfile) {
+        mRepository.insert(squeakProfile);
+    }
+
+    public LiveData<Signing.KeyPair> getKeyPair() {
+        return Transformations.map(mECKey, key -> {
+            return new Signing.BitcoinjKeyPair(key);
+        });
+    }
+
+}
