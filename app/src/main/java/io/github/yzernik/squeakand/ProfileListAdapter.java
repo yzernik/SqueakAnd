d21a464de91829490921f48b4c256f7b1712b2ed
yzernik <yzernik@gmail.com>
2020-05-19 02:09:23 -0700

    Add recycler view for manage profiles fragment

---
+++ app/src/main/java/io/github/yzernik/squeakand/ProfileListAdapter.java
@@ -0,0 +1,78 @@
+package io.github.yzernik.squeakand;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProfileListAdapter extends RecyclerView.Adapter<ProfileListAdapter.ProfileViewHolder> {
+
+    class ProfileViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtProfileName;
+
+        public ProfileViewHolder(View view) {
+            super(view);
+
+            txtProfileName = view.findViewById(R.id.profile_item_view_name);
+
+            txtProfileName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.handleItemClick(mProfiles.get(getAdapterPosition()).getProfileId());
+                }
+            });
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<SqueakProfile> mProfiles; // Cached copy of profiles
+    private ClickListener clickListener;
+
+    public ProfileListAdapter(Context context, ClickListener clickListener) {
+        mInflater = LayoutInflater.from(context);
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public ProfileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.profile_item_layout, parent, false);
+        return new ProfileViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ProfileViewHolder holder, int position) {
+        if (mProfiles != null) {
+            SqueakProfile current = mProfiles.get(position);
+            holder.txtProfileName.setText(current.getName());
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.txtProfileName.setText("No profile");
+        }
+    }
+
+    public void setProfiles(List<SqueakProfile> profiles) {
+        mProfiles = profiles;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mTodos has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mProfiles != null)
+            return mProfiles.size();
+        else return 0;
+    }
+
+
+    public interface ClickListener {
+        void handleItemClick(int id);
+    }
+
+}
--- app/src/main/java/io/github/yzernik/squeakand/ui/profile/ManageProfilesFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/profile/ManageProfilesFragment.java
@@ -2,6 +2,7 @@ package io.github.yzernik.squeakand.ui.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +15,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import io.github.yzernik.squeakand.NewProfileActivity;
+import io.github.yzernik.squeakand.ProfileListAdapter;
 import io.github.yzernik.squeakand.R;
 import io.github.yzernik.squeakand.SqueakProfile;
+import io.github.yzernik.squeakand.Todo;
+import io.github.yzernik.squeakand.TodoListAdapter;
+import io.github.yzernik.squeakand.ViewTodoActivity;
 
 
-public class ManageProfilesFragment extends Fragment {
+public class ManageProfilesFragment extends Fragment implements ProfileListAdapter.ClickListener{
 
     public static final int NEW_PROFILE_ACTIVITY_REQUEST_CODE = 1;
 
@@ -36,8 +43,13 @@ public class ManageProfilesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_manage_profiles, container, false);
 
+        final RecyclerView recyclerView = root.findViewById(R.id.profilesRecyclerView);
+        final ProfileListAdapter adapter = new ProfileListAdapter(root.getContext(), this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+
+
         mCreateProfileButton = root.findViewById(R.id.create_profile_button);
-        mProfilesSpinner = root.findViewById(R.id.profiles_spinner);
 
         manageProfilesModel = new ViewModelProvider(getActivity()).get(ManageProfilesModel.class);
 
@@ -49,7 +61,7 @@ public class ManageProfilesFragment extends Fragment {
             }
         });
 
-        manageProfilesModel.getmAllSqueakProfiles().observe(getViewLifecycleOwner(), new Observer<List<SqueakProfile>>() {
+/*        manageProfilesModel.getmAllSqueakProfiles().observe(getViewLifecycleOwner(), new Observer<List<SqueakProfile>>() {
             @Override
             public void onChanged(@Nullable final List<SqueakProfile> squeakProfiles) {
                 List<String> profileNames = squeakProfiles.stream()
@@ -59,9 +71,23 @@ public class ManageProfilesFragment extends Fragment {
                         android.R.layout.simple_spinner_item, profileNames);
                 mProfilesSpinner.setAdapter(spinnerArrayAdapter);
             }
+        });*/
+
+        manageProfilesModel.getmAllSqueakProfiles().observe(getViewLifecycleOwner(), new Observer<List<SqueakProfile>>() {
+            @Override
+            public void onChanged(@Nullable final List<SqueakProfile> squeakProfiles) {
+                // Update the cached copy of the profiles in the adapter.
+                adapter.setProfiles(squeakProfiles);
+            }
         });
 
         return root;
+    }
+
+    @Override
+    public void handleItemClick(int id) {
+        // TODO: go to profile activity
+        Log.i(getTag(), "Clicked on profile id: " + id);
     }
 
 }
