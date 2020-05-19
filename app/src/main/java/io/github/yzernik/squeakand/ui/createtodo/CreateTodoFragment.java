3cb3dc036bd88898fc767d086dddf8df00990ff7
yzernik <yzernik@gmail.com>
2020-05-18 23:24:56 -0700

    Rearrange buttons in create todo layout

--- app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/createtodo/CreateTodoFragment.java
@@ -48,6 +48,7 @@ public class CreateTodoFragment extends Fragment {
         mManageProfilesButton = root.findViewById(R.id.manage_profiles_button);
         mTextInput = root.findViewById(R.id.squeak_text);
         button = root.findViewById(R.id.btnDone);
+        mTextInput.requestFocus();
 
         createTodoModel = new ViewModelProvider(getActivity()).get(CreateTodoModel.class);
 
