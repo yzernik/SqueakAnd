477d15c7b61813d0085cf1b25b5823242fef691c
yzernik <yzernik@gmail.com>
2020-05-19 18:29:20 -0700

    Genesis block height should be zero

--- app/src/main/java/io/github/yzernik/squeakand/blockchain/DummyBlockchain.java
+++ app/src/main/java/io/github/yzernik/squeakand/blockchain/DummyBlockchain.java
@@ -7,7 +7,7 @@ import org.bitcoinj.core.Sha256Hash;
 
 public class DummyBlockchain implements Blockchain {
     private static final Sha256Hash GENESIS_BLOCK_HASH = Sha256Hash.wrap("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
-    private static final int GENESIS_BLOCK_HEIGHT = 1;
+    private static final int GENESIS_BLOCK_HEIGHT = 0;
 
     @Override
     public BlockInfo getLatestBlock() {
