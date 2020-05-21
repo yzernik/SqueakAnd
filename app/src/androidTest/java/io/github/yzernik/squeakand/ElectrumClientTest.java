d676add90794d69a9ad7c9a019f7ff61e01146a3
yzernik <yzernik@gmail.com>
2020-05-21 15:51:43 -0700

    Clean up imports in electrum client test

--- app/src/androidTest/java/io/github/yzernik/squeakand/ElectrumClientTest.java
+++ app/src/androidTest/java/io/github/yzernik/squeakand/ElectrumClientTest.java
@@ -1,10 +1,6 @@
 package io.github.yzernik.squeakand;
 
-import android.content.Context;
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.room.Room;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
@@ -13,17 +9,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.net.URL;
-import java.util.List;
 import java.util.stream.Stream;
 
-import io.github.yzernik.squeakand.blockchain.Blockchain;
-import io.github.yzernik.squeakand.blockchain.DummyBlockchain;
 import io.github.yzernik.squeakand.blockchain.ElectrumClient;
-import io.github.yzernik.squeaklib.core.Signing;
-import io.github.yzernik.squeaklib.core.Squeak;
-
-import static junit.framework.Assert.assertEquals;
 
 
 @RunWith(AndroidJUnit4.class)
