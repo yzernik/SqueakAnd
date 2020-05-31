973d2f45dbaf5c31b0e74bdb195bbec57526a5c3
yzernik <yzernik@gmail.com>
2020-05-30 19:04:08 -0700

    Rename fragment and models etc. to electrum from blockchain

--- app/src/main/java/io/github/yzernik/squeakand/ElectrumActivity.java
+++ app/src/main/java/io/github/yzernik/squeakand/ElectrumActivity.java
@@ -6,17 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import io.github.yzernik.squeakand.ui.blockchain.BlockchainFragment;
+import io.github.yzernik.squeakand.ui.electrum.ElectrumFragment;
 
-public class BlockchainActivity extends AppCompatActivity {
+public class ElectrumActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blockchain);
+        setContentView(R.layout.activity_electrum);
 
         // Create new fragment and transaction
-        Fragment newFragment = new BlockchainFragment();
+        Fragment newFragment = new ElectrumFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         // Replace whatever is in the fragment_container view with this fragment,
         // and add the transaction to the back stack
