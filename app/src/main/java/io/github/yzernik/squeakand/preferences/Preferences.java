cab7f09c43a46712e3e14fb066cb455aeed64b55
yzernik <yzernik@gmail.com>
2020-05-31 19:31:34 -0700

    Create preferences class to handle sharedpreferences

---
+++ app/src/main/java/io/github/yzernik/squeakand/preferences/Preferences.java
@@ -0,0 +1,29 @@
+package io.github.yzernik.squeakand.preferences;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Preferences {
+
+    private static final String SQUEAK_PROFILE_FILE_KEY = "io.github.yzernik.squeakand.SQUEAK_PREFERENCES";
+    private static final String SELECTED_SQUEAK_PROFILE_ID_KEY = "SELECTED_SQUEAK_PROFILE_ID";
+
+    private SharedPreferences sharedPreferences;
+
+    public Preferences(Application application) {
+        sharedPreferences = application.getSharedPreferences(
+                SQUEAK_PROFILE_FILE_KEY, Context.MODE_PRIVATE);
+    }
+
+    public int getProfileId() {
+        return sharedPreferences.getInt(SELECTED_SQUEAK_PROFILE_ID_KEY, -1);
+    }
+
+    public void saveSelectedProfileId(int squeakProfileId) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(SELECTED_SQUEAK_PROFILE_ID_KEY, squeakProfileId);
+        editor.commit();
+    }
+
+}
