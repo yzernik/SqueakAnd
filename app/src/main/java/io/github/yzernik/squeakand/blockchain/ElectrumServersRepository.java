a1af35680cf7c1b3a72abc398131bf04b801ce58
yzernik <yzernik@gmail.com>
2020-06-01 01:49:10 -0700

    Create LiveElectrumPeersMap to handle hashmap with livedata list of servers

--- app/src/main/java/io/github/yzernik/squeakand/blockchain/ElectrumServersRepository.java
+++ app/src/main/java/io/github/yzernik/squeakand/blockchain/ElectrumServersRepository.java
@@ -11,13 +11,14 @@ public class ElectrumServersRepository {
     private static volatile ElectrumServersRepository INSTANCE;
 
     private MutableLiveData<List<ElectrumServerAddress>> liveServers = new MutableLiveData<>();
+    private LiveElectrumPeersMap peersMap = new LiveElectrumPeersMap(liveServers);
 
     private PeerDownloader peerDownloader;
 
     private ElectrumServersRepository() {
         // Singleton constructor, only called by static method.
         System.out.println("Starting new ElectrumServersRepository");
-        peerDownloader = new PeerDownloader(liveServers);
+        peerDownloader = new PeerDownloader(peersMap);
     }
 
     public static ElectrumServersRepository getRepository() {
