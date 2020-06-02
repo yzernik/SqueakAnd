9e1b717d09698e75a3114a6ab4cc2dd436938d18
yzernik <yzernik@gmail.com>
2020-06-01 23:41:11 -0700

    Use application class to initialize electrum repository instead of main activity

---
+++ app/src/main/java/io/github/yzernik/squeakand/SqueakAndApplication.java
@@ -0,0 +1,44 @@
+package io.github.yzernik.squeakand;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import io.github.yzernik.squeakand.blockchain.ElectrumBlockchainRepository;
+
+public class SqueakAndApplication extends Application {
+    // Called when the application is starting, before any other application objects have been created.
+    // Overriding this method is totally optional!
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Required initialization logic here!
+
+        // Initialize the network connections.
+        initializeNetworkConnections();
+    }
+
+    // Called by the system when the device configuration changes while your component is running.
+    // Overriding this method is totally optional!
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    // This is called when the overall system is running low on memory,
+    // and would like actively running processes to tighten their belts.
+    // Overriding this method is totally optional!
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    /**
+     * Initializes the connection to electrum server, etc.
+     */
+    private void initializeNetworkConnections() {
+        ElectrumBlockchainRepository electrumBlockchainRepository = ElectrumBlockchainRepository.getRepository(this);
+        Log.i(getClass().getName(), "Calling electrumBlockchainRepository.initialize...");
+        electrumBlockchainRepository.initialize();
+    }
+}
