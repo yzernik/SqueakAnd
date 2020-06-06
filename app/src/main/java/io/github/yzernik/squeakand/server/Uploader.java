14a4aabee7800773a37a13211994325c81092524
yzernik <yzernik@gmail.com>
2020-06-05 20:33:50 -0700

    Syncer uploads squeaks that are not on server

--- app/src/main/java/io/github/yzernik/squeakand/server/Uploader.java
+++ app/src/main/java/io/github/yzernik/squeakand/server/Uploader.java
@@ -2,15 +2,23 @@ package io.github.yzernik.squeakand.server;
 
 import android.util.Log;
 
+import com.google.common.collect.Lists;
+
 import org.bitcoinj.core.Sha256Hash;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import io.github.yzernik.squeakand.SqueakDao;
+import io.github.yzernik.squeakand.SqueakEntry;
 import io.github.yzernik.squeakand.client.SqueakRPCClient;
 import io.github.yzernik.squeaklib.core.Squeak;
 
 public class Uploader {
+
+    private static final int DEFAULT_MIN_BLOCK = 0;
+    private static final int DEFAULT_MAX_BLOCK = 1000000000;
 
     private final SqueakServerAddress serverAddress;
     private final SqueakDao squeakDao;
@@ -23,22 +31,45 @@ public class Uploader {
 
     }
 
-    public void upload(Squeak squeak) throws InterruptedException {
+    public void upload(Squeak squeak) {
         // TODO: implement
         // client.postSqueak(squeak);
     }
 
-    public void uploadSync(List<String> uploadAddresses) {
+    public void uploadSync(List<String> uploadAddresses, int minBlock, int maxBlock) {
         Log.i(getClass().getName(), "Calling uploadSync...");
-        try {
-            List<Sha256Hash> serverHashes = client.lookupSqueaks(uploadAddresses, 0, 0);
-            Log.i(getClass().getName(), "Got server hashes: " + serverHashes);
-        } catch (Exception e) {
-            Log.i(getClass().getName(), "Got exception : " + e);
+
+        Set<Sha256Hash> remoteHashes = getRemoteHashes(uploadAddresses, minBlock, maxBlock);
+        Log.i(getClass().getName(), "Got server number of hashes: " + remoteHashes.size());
+
+        Set<Sha256Hash> localHashes = getLocalHashes(uploadAddresses, minBlock, maxBlock);
+        Log.i(getClass().getName(), "Got local number of hashes: " + remoteHashes.size());
+
+        // For every local hash not in server hashes, upload.
+        localHashes.removeAll(remoteHashes);
+        Log.i(getClass().getName(), "Uploading number of squeaks: " + localHashes.size());
+        for (Sha256Hash hash: localHashes) {
+            Squeak squeak = squeakDao.fetchSqueakByHash(hash).getValue().getSqueak();
+            upload(squeak);
         }
 
-        // TODO: check against database, and upload the missing hashes
-        // List<Sha256Hash> localHashes = squeakDao.fetchSqueakByAddresses(uploadAddresses);
-        // For every local hash not in serverhashes, upload.
+    }
+
+    private Set<Sha256Hash> getRemoteHashes(List<String> uploadAddresses, int minBlock, int maxBlock) {
+        List<Sha256Hash> hashList = client.lookupSqueaks(uploadAddresses, minBlock, maxBlock);
+        return new HashSet<>(hashList);
+    }
+
+
+    private Set<Sha256Hash> getLocalHashes(List<String> uploadAddresses, int minBlock, int maxBlock) {
+        Set<Sha256Hash> localHashes = new HashSet<>();
+        for (String address: uploadAddresses) {
+            // TODO: include block range in DAO method.
+            List<SqueakEntry> squeakEntries = squeakDao.fetchSqueaksByAddress(address).getValue();
+            for (SqueakEntry squeakEntry: squeakEntries) {
+                localHashes.add(squeakEntry.hash);
+            }
+        }
+        return localHashes;
     }
 }
