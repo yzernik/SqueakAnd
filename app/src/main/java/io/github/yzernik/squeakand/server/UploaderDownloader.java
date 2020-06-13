8fac2a5147d983f71d29271bcc6933e51227f4c1
Jonathan Zernik <yzernik@gmail.com>
2020-06-12 23:00:27 -0700

    Improve squeak display in timeline (#156)
    * Remove author address from author display name in timeline
    
    * Show profile name in view squeak ui

--- app/src/main/java/io/github/yzernik/squeakand/server/UploaderDownloader.java
+++ app/src/main/java/io/github/yzernik/squeakand/server/UploaderDownloader.java
@@ -51,7 +51,7 @@ public class UploaderDownloader {
         localHashes.removeAll(remoteHashes);
         Log.i(getClass().getName(), "Uploading number of squeaks: " + localHashes.size());
         for (Sha256Hash hash: localHashes) {
-            Squeak squeak = squeakDao.fetchSqueakByHash(hash).getSqueak();
+            Squeak squeak = squeakDao.fetchSqueakByHash(hash).squeakEntry.getSqueak();
             upload(squeak);
         }
     }
