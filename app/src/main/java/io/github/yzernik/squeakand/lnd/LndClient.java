1ac7de06c4ea134551b74d3e5f210b12b87739f7
Jonathan Zernik <yzernik@gmail.com>
2020-06-17 00:25:56 -0700

    Add lnd aar (#168)
    * Add lnd mobile aar file
    
    * Successfully import lndmobile
    
    * Try to start lnd node, but crashes
    
    * Undo change to android manifest
    
    * Use lnd config in starting lnd node
    
    * Fix lnddir config

---
+++ app/src/main/java/io/github/yzernik/squeakand/lnd/LndClient.java
@@ -0,0 +1,60 @@
+package io.github.yzernik.squeakand.lnd;
+
+import android.util.Log;
+
+import lndmobile.Callback;
+import lndmobile.Lndmobile;
+
+import static org.bitcoinj.core.Utils.HEX;
+
+public class LndClient {
+
+    private static final String DEFAULT_NETWORK = "testnet";
+
+    private final String network;
+    private final String lndDirPath;
+
+    public LndClient(String lndDirPath, String network) {
+        this.lndDirPath = lndDirPath;
+        this.network = network;
+    }
+
+    public LndClient(String lndDirPath) {
+        this(lndDirPath, DEFAULT_NETWORK);
+    }
+
+    public void start() {
+        String startString = String.format(
+                "--bitcoin.active --bitcoin.node=neutrino --bitcoin.%s --no-macaroons --lnddir=%s",
+                network,
+                lndDirPath);
+
+        Log.i(getClass().getName(), "Starting lndmobile with lndDir: " + lndDirPath);
+        Log.i(getClass().getName(), "Start string : " + startString);
+        Lndmobile.start(
+                startString,
+                new Callback() {
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e(getClass().getName(), "Error from callback1: " + e);
+                    }
+
+                    @Override
+                    public void onResponse(byte[] bytes) {
+                        Log.i(getClass().getName(), "Response from callback1: " + HEX.encode(bytes));
+                    }
+                },
+                new Callback() {
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e(getClass().getName(), "Error from callback1: " + e);
+                    }
+
+                    @Override
+                    public void onResponse(byte[] bytes) {
+                        Log.i(getClass().getName(), "Response from callback2: " + HEX.encode(bytes));
+                    }
+                });
+    }
+
+}
