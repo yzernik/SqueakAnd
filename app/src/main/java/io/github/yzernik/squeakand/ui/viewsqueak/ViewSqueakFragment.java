77c0c63382ec51ab6e765c844c9a054cfb2dbc97
yzernik <yzernik@gmail.com>
2020-06-18 22:28:21 -0700

    Squeak detail page shows full text and block time

--- app/src/main/java/io/github/yzernik/squeakand/ui/viewsqueak/ViewSqueakFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/viewsqueak/ViewSqueakFragment.java
@@ -5,25 +5,33 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import org.bitcoinj.core.Sha256Hash;
 
+import java.util.Date;
+
 import io.github.yzernik.squeakand.R;
 import io.github.yzernik.squeakand.SqueakEntryWithProfile;
+import io.github.yzernik.squeakand.SqueakProfile;
+import io.github.yzernik.squeakand.TimeUtil;
 
 public class ViewSqueakFragment extends Fragment {
 
-    TextView txtSqueakAddress;
-    TextView txtSqueakHash;
-    TextView txtSqueakText;
-    TextView txtSqueakAuthor;
+    private TextView txtSqueakAddress;
+    private TextView txtSqueakAuthor;
+    private TextView txtSqueakText;
+    private TextView txtSqueakBlock;
+    private CardView squeakCardView;
+    private LinearLayout squeakAddressBox;
 
     // private EditText mEditTodoView;
     private ViewSqueakModel todoViewModel;
@@ -43,9 +51,11 @@ public class ViewSqueakFragment extends Fragment {
         todoViewModel = new ViewModelProvider(this).get(ViewSqueakModel.class);
 
         txtSqueakAddress = root.findViewById(R.id.squeak_item_address);
-        txtSqueakHash = root.findViewById(R.id.squeak_author);
+        txtSqueakAuthor = root.findViewById(R.id.squeak_author);
         txtSqueakText = root.findViewById(R.id.squeak_text);
-        txtSqueakAuthor = root.findViewById(R.id.squeak_block);
+        txtSqueakBlock = root.findViewById(R.id.squeak_block);
+        squeakCardView = root.findViewById(R.id.squeakCardView);
+        squeakAddressBox = root.findViewById(R.id.squeak_address_box);
 
         todoViewModel.getSingleTodo(squeakHash).observe(getViewLifecycleOwner(), new Observer<SqueakEntryWithProfile>() {
             @Override
@@ -53,6 +63,8 @@ public class ViewSqueakFragment extends Fragment {
                 if (squeakEntryWithProfile == null) {
                     return;
                 }
+
+                /*
                 Log.i(getTag(), "Viewing squeak: " + squeakEntryWithProfile.squeakEntry.getSqueak());
                 Log.i(getTag(), "Viewing squeak block: " + squeakEntryWithProfile.squeakEntry.getBlock());
 
@@ -62,11 +74,33 @@ public class ViewSqueakFragment extends Fragment {
                 }
                 txtSqueakHash.setText(authorDisplayString);
                 txtSqueakText.setText(squeakEntryWithProfile.squeakEntry.getDecryptedContentStr());
-                txtSqueakAuthor.setText("Block #" + String.valueOf(squeakEntryWithProfile.squeakEntry.blockHeight));
+                txtSqueakAuthor.setText("Block #" + String.valueOf(squeakEntryWithProfile.squeakEntry.blockHeight));*/
+
+                txtSqueakAuthor.setText(getAuthorDisplay(squeakEntryWithProfile));
+                txtSqueakText.setText(squeakEntryWithProfile.squeakEntry.getDecryptedContentStr());
+                txtSqueakBlock.setText(getBlockDisplay(squeakEntryWithProfile));
+                txtSqueakAddress.setText(squeakEntryWithProfile.squeakEntry.authorAddress);
             }
         });
 
         return root;
+    }
+
+    private String getBlockDisplay(SqueakEntryWithProfile currentEntry) {
+        long blockNumber = currentEntry.squeakEntry.blockHeight;
+        Date blockTime = currentEntry.squeakEntry.block.getTime();
+        return String.format("Block #%d (%s)", blockNumber, blockTime.toString());
+    }
+
+    private String getAuthorDisplay(SqueakEntryWithProfile currentEntry) {
+        String authorAddress = currentEntry.squeakEntry.authorAddress;
+        SqueakProfile authorProfile = currentEntry.squeakProfile;
+        String authorDisplay = authorAddress;
+        if (authorProfile != null) {
+            String authorName = currentEntry.squeakProfile.getName();
+            authorDisplay = authorName;
+        }
+        return authorDisplay;
     }
 
 }
