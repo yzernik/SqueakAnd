38ea2e8ef439fb643faca47df92283c15b045792
yzernik <yzernik@gmail.com>
2020-06-19 22:14:22 -0700

    Fix author name display in squeak detail

--- app/src/main/java/io/github/yzernik/squeakand/SqueakDisplayUtil.java
+++ app/src/main/java/io/github/yzernik/squeakand/SqueakDisplayUtil.java
@@ -5,9 +5,14 @@ import java.util.Date;
 public class SqueakDisplayUtil {
 
     public static String getAuthorText(SqueakEntryWithProfile entryWithProfile) {
-        long blockNumber = entryWithProfile.squeakEntry.blockHeight;
-        Date blockTime = entryWithProfile.squeakEntry.block.getTime();
-        return String.format("Block #%d (%s)", blockNumber, blockTime.toString());
+        String authorAddress = entryWithProfile.squeakEntry.authorAddress;
+        SqueakProfile authorProfile = entryWithProfile.squeakProfile;
+        String authorDisplay = authorAddress;
+        if (authorProfile != null) {
+            String authorName = entryWithProfile.squeakProfile.getName();
+            authorDisplay = authorName;
+        }
+        return authorDisplay;
     }
 
     public static String getSqueakText(SqueakEntryWithProfile entryWithProfile) {
