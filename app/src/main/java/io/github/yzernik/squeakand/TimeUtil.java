b3821ad2f3140236d4c053b354f50be4250a5496
yzernik <yzernik@gmail.com>
2020-06-21 14:37:12 -0700

    Fix time unit when singular

--- app/src/main/java/io/github/yzernik/squeakand/TimeUtil.java
+++ app/src/main/java/io/github/yzernik/squeakand/TimeUtil.java
@@ -14,13 +14,49 @@ public class TimeUtil {
         Duration duration = getDuration(initialTime, finalTime);
 
         if (duration.toMinutes() < 1) {
-            return String.format("%d seconds ago", duration.getSeconds());
+            return timeAgoSeconds(duration);
         } else if (duration.toHours() < 1) {
-            return String.format("%d minutes ago", duration.toMinutes());
+            return timeAgoMinutes(duration);
         } else if (duration.toDays() < 1) {
-            return String.format("%d hours ago", duration.toHours());
+            return timeAgoHours(duration);
         } else {
-            return String.format("%d days ago", duration.toDays());
+            return timeAgoDays(duration);
+        }
+    }
+
+    private static String timeAgoSeconds(Duration duration) {
+        long seconds = duration.getSeconds();
+        if (seconds == 1) {
+            return "1 second ago";
+        } else {
+            return String.format("%d seconds ago", seconds);
+        }
+    }
+
+    private static String timeAgoMinutes(Duration duration) {
+        long minutes = duration.toMinutes();
+        if (minutes == 1) {
+            return "1 minute ago";
+        } else {
+            return String.format("%d minutes ago", minutes);
+        }
+    }
+
+    private static String timeAgoHours(Duration duration) {
+        long hours = duration.toHours();
+        if (hours == 1) {
+            return "1 hour ago";
+        } else {
+            return String.format("%d hours ago", hours);
+        }
+    }
+
+    private static String timeAgoDays(Duration duration) {
+        long days = duration.toDays();
+        if (days == 1) {
+            return "1 day ago";
+        } else {
+            return String.format("%d days ago", days);
         }
     }
 
