3d3624cfdffd343c705aef34e81da1ae2d64e69f
yzernik <yzernik@gmail.com>
2020-06-21 18:58:13 -0700

    Simplify view squeak model

--- app/src/main/java/io/github/yzernik/squeakand/ui/viewsqueak/ViewSqueakModel.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/viewsqueak/ViewSqueakModel.java
@@ -9,32 +9,31 @@ import org.bitcoinj.core.Sha256Hash;
 
 import java.util.List;
 
-import io.github.yzernik.squeakand.SqueakEntry;
 import io.github.yzernik.squeakand.SqueakEntryWithProfile;
 import io.github.yzernik.squeakand.SqueakRepository;
 
 public class ViewSqueakModel extends AndroidViewModel {
 
     private Sha256Hash squeakHash;
+    private LiveData<SqueakEntryWithProfile> liveSqueak;
+    private LiveData<List<SqueakEntryWithProfile>> liveThreadAncestorSqueaks;
 
     private SqueakRepository mRepository;
-    // Using LiveData and caching what fetchTodoById returns has several benefits:
-    // - We can put an observer on the data (instead of polling for changes) and only update the
-    //   the UI when the data actually changes.
-    // - Repository is completely separated from the UI through the ViewModel.
 
     public ViewSqueakModel(Application application, Sha256Hash squeakHash) {
         super(application);
         mRepository = SqueakRepository.getRepository(application);
         this.squeakHash = squeakHash;
+        this.liveSqueak = mRepository.getSqueak(squeakHash);
+        this.liveThreadAncestorSqueaks = mRepository.getThreadAncestorSqueaks(squeakHash);
     }
 
-    public LiveData<SqueakEntryWithProfile> getSingleTodo() {
-        return mRepository.getSqueak(squeakHash);
+    public LiveData<SqueakEntryWithProfile> getSqueak() {
+        return liveSqueak;
     }
 
     public LiveData<List<SqueakEntryWithProfile>> getThreadAncestorSqueaks() {
-        return mRepository.getThreadAncestorSqueaks(squeakHash);
+        return liveThreadAncestorSqueaks;
     }
 
 }
