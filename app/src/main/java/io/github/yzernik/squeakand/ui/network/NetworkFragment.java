64098dbf5cc1d0c73c28f390b787db036d09b6ea
yzernik <yzernik@gmail.com>
2020-06-21 17:13:25 -0700

    Optimize imports

--- app/src/main/java/io/github/yzernik/squeakand/ui/network/NetworkFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/network/NetworkFragment.java
@@ -1,36 +1,19 @@
 package io.github.yzernik.squeakand.ui.network;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.List;
-
 import io.github.yzernik.squeakand.R;
-import io.github.yzernik.squeakand.ServerListAdapter;
-import io.github.yzernik.squeakand.SqueakProfile;
-import io.github.yzernik.squeakand.SqueakServer;
-import io.github.yzernik.squeakand.ViewServerActivity;
-import io.github.yzernik.squeakand.server.SqueakServerAddress;
 
 public class NetworkFragment extends Fragment {
     // When requested, this adapter returns a DemoObjectFragment,
