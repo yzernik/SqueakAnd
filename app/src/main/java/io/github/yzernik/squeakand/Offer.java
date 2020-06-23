3561877eec807b58af1bcb69b314fbb0f3cd0298
yzernik <yzernik@gmail.com>
2020-06-22 18:21:53 -0700

    Add buysqueak request to client

--- app/src/main/java/io/github/yzernik/squeakand/Offer.java
+++ app/src/main/java/io/github/yzernik/squeakand/Offer.java
@@ -2,16 +2,66 @@ package io.github.yzernik.squeakand;
 
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
 
 import org.bitcoinj.core.Sha256Hash;
 
+import io.github.yzernik.squeakand.networkparameters.NetworkParameters;
+import io.github.yzernik.squeaklib.core.Signing;
+
 @Entity(tableName = SqueakRoomDatabase.TABLE_NAME_OFFER,
         indices = {@Index(value = {"squeakHash"}, unique = true), @Index(value = {"squeakServerId"}, unique = true)})
 @TypeConverters({Converters.class})
 public class Offer {
+
+    public Offer() {
+    }
+
+    @Ignore
+    public Offer(
+            Sha256Hash squeakHash,
+            byte[] nonce,
+            Sha256Hash preimageHash,
+            long amount,
+            String paymentRequest,
+            String pubkey,
+            String host,
+            int port,
+            int squeakServerId,
+            byte[] preimage) {
+        this.squeakHash = squeakHash;
+        this.nonce = nonce;
+        this.preimageHash = preimageHash;
+        this.amount = amount;
+        this.paymentRequest = paymentRequest;
+        this.pubkey = pubkey;
+        this.host = host;
+        this.port = port;
+        this.squeakServerId = squeakServerId;
+        this.preimage = preimage;
+    }
+
+    @Ignore
+    public Offer(
+            Sha256Hash squeakHash,
+            byte[] nonce,
+            Sha256Hash preimageHash,
+            long amount,
+            String paymentRequest,
+            String pubkey,
+            String host,
+            int port,
+            int squeakServerId) {
+        this(squeakHash, nonce, preimageHash, amount, paymentRequest, pubkey, host, port, squeakServerId, null);
+    }
+
+    @Ignore
+    public void setSqueakServerId(int squeakServerId) {
+        this.squeakServerId = squeakServerId;
+    }
 
     @PrimaryKey(autoGenerate = true)
     public int offerId;
