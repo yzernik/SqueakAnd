9621fe81a09628d0272f3196a95c97ad09818439
yzernik <yzernik@gmail.com>
2020-06-28 01:06:18 -0700

    Show connection status of lightning node in lightning node fragment in connection tab

--- app/src/main/java/io/github/yzernik/squeakand/ui/lightningnode/LightningNodeConnectionFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/lightningnode/LightningNodeConnectionFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.util.List;
+
 import io.github.yzernik.squeakand.R;
+import lnrpc.Rpc;
 
 public class LightningNodeConnectionFragment extends Fragment {
+
+    private TextView mLightningNodePubkeyText;
+    private TextView mLightningNodeConnectionStatusText;
 
     private LightningNodeConnectionModel lightningNodeChannelsModel;
 
@@ -27,9 +36,21 @@ public class LightningNodeConnectionFragment extends Fragment {
             host = this.getArguments().getString("host");
         }
 
+        mLightningNodePubkeyText = root.findViewById(R.id.lightning_node_pubkey);
+        mLightningNodeConnectionStatusText = root.findViewById(R.id.lightning_node_connection_status_text);
+
         lightningNodeChannelsModel = ViewModelProviders.of(this,
                 new LightningNodeConnectionModelFactory(getActivity().getApplication(), pubkey, host))
                 .get(LightningNodeConnectionModel.class);
+
+        mLightningNodePubkeyText.setText("Pubkey: " + pubkey);
+
+        lightningNodeChannelsModel.liveIsPeerConnected().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable final Boolean isPeerConnected) {
+                mLightningNodeConnectionStatusText.setText("Is peer connected: " + isPeerConnected);
+            }
+        });
 
         return root;
     }
