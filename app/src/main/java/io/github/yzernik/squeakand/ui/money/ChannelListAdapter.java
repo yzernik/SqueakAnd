1fd8e449823b1561a4a9d3103c1a7b337fada825
yzernik <yzernik@gmail.com>
2020-06-28 01:49:51 -0700

    Fix capitalization of channels list adapter fields

--- app/src/main/java/io/github/yzernik/squeakand/ui/money/ChannelListAdapter.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/money/ChannelListAdapter.java
@@ -74,7 +74,7 @@ public class ChannelListAdapter extends RecyclerView.Adapter<ChannelListAdapter.
         if (mChannels != null) {
             Rpc.Channel current = mChannels.get(position);
             holder.txtChannelPubkey.setText("Pubkey: " + current.getRemotePubkey());
-            holder.txtChannelIsActive.setText("Is Active: " + current.getActive());
+            holder.txtChannelIsActive.setText("Is active: " + current.getActive());
             holder.txtChannelCapacity.setText("Capacity: " + current.getCapacity());
             holder.txtChannelLocalBalance.setText("Local balance: " + current.getLocalBalance());
             holder.txtChannelRemoteBalance.setText("Remote balance: " + current.getRemoteBalance());
