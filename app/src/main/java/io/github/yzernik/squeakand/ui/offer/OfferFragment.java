e509fbd87aa1cd5ccc1b32d1fa891a6fd5ca64b3
yzernik <yzernik@gmail.com>
2020-06-28 19:58:21 -0700

    Show squeak, server, and lightning node in offer ui

--- app/src/main/java/io/github/yzernik/squeakand/ui/offer/OfferFragment.java
+++ app/src/main/java/io/github/yzernik/squeakand/ui/offer/OfferFragment.java
@@ -28,11 +28,10 @@ import lnrpc.Rpc;
 public class OfferFragment extends Fragment {
 
     private TextView txtOfferPrice;
-    private TextView txtOfferSqueakHash;
-    private TextView txtOfferServerAddress;
-    private TextView txtOfferLightningAddress;
-    private Button btnPay;
+    private Button btnViewSqueak;
+    private Button btnViewServer;
     private Button btnViewLightningNode;
+    private Button btnPay;
 
 
     private OfferModel offerModel;
@@ -54,11 +53,11 @@ public class OfferFragment extends Fragment {
                 .get(OfferModel.class);
 
         txtOfferPrice = root.findViewById(R.id.offer_price_text);
-        txtOfferSqueakHash = root.findViewById(R.id.offer_squeak_hash_text);
-        txtOfferServerAddress = root.findViewById(R.id.offer_server_address_text);
-        txtOfferLightningAddress = root.findViewById(R.id.offer_lightning_address_text);
+        btnViewSqueak = root.findViewById(R.id.offer_view_squeak_button);
+        btnViewServer = root.findViewById(R.id.offer_view_server_button);
+        btnViewLightningNode = root.findViewById(R.id.offer_view_lightning_node_button);
         btnPay = root.findViewById(R.id.offer_pay_button);
-        btnViewLightningNode = root.findViewById(R.id.offer_view_lightning_node);
+        // btnViewLightningNode = root.findViewById(R.id.offer_view_lightning_node);
 
         btnPay.setVisibility(View.GONE);
         btnViewLightningNode.setVisibility(View.GONE);
@@ -75,9 +74,9 @@ public class OfferFragment extends Fragment {
 
                 Log.i(getTag(), "Got offer: " + offer);
                 txtOfferPrice.setText("Price: " + offer.getAmount() + " satoshis");
-                txtOfferSqueakHash.setText("Squeak hash: " + offer.getSqueakHash());
-                txtOfferServerAddress.setText("Server: " + squeakServer.serverAddress);
-                txtOfferLightningAddress.setText("Lightning address: " + offer.getLightningAddress());
+                btnViewSqueak.setText(offer.getSqueakHash().toString());
+                btnViewServer.setText(squeakServer.serverAddress.toString());
+                btnViewLightningNode.setText(offer.getLightningAddress());
 
                 // Set up view lightning node button
                 btnViewLightningNode.setOnClickListener(new View.OnClickListener() {
@@ -152,7 +151,7 @@ public class OfferFragment extends Fragment {
         String msg = "Failed with error: " + error;
 
         if (error.equals("insufficient_balance")) {
-            msg += "\nHint: Try opening a new channel to increase your local balance.";
+            msg += "\nHint: Try opening a new channel to the lightning node to increase your local balance.";
         }
 
         alertDialog.setMessage(msg);
